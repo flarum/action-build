608c28bd3b997de6e059170ee7284d2942b117a3
David Sevilla Martin <me@datitisev.me>
2019-08-20 14:43:39 -0400

    Check if anything needs to be commited before creating commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,22 @@ git config user.email 'bot@flarum.org'
 
 cd js
 
+echo -e '\e[36m\e[1mInstalling dependencies.'
+
 npm i -g npm@6.1.0
 npm ci
+
+echo -e '\e[36m\e[1mBuilding JavaScript files.'
+
 npm run build
 
 git add dist/* -f
-git commit -m "Bundled output for commit $GITHUB_SHA [skip ci]"
 
-git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" "HEAD:$GITHUB_REF"
+if [[ -z $(git status -uno --porcelain) ]]; then
+    echo -e '\e[36m\e[1mNothing to commit.'
+else
+    echo -e '\e[36m\e[1mCommitting and pushing.'
+
+    git commit -m "Bundled output for commit $GITHUB_SHA [skip ci]"
+    git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" "HEAD:$GITHUB_REF"
+fi
