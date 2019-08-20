4ce9be9aed51f57629865632dfe45ed55c5c2599
David Sevilla Martin <me@datitisev.me>
2019-08-20 12:24:34 -0400

    Use remote origin instead of direct push to URL

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,5 @@ npm run build
 git add dist/* -f
 git commit -m "Bundled output for commit $GITHUB_SHA [skip ci]"
 
-GITHUB_BRANCH=`git rev-parse --abbrev-ref HEAD`
-
-git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" $GITHUB_BRANCH
+git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git push
