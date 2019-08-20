f5812728a133e7e6327d26f32f9b3fcb66428f3f
David Sevilla Martin <me@datitisev.me>
2019-08-20 12:06:50 -0400

    Initial script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/sh -l
 
-echo "hello $1"
+set -e
+
+if [ -z $GITHUB_TOKEN ]; then
+    echo -e '\033[0;31mGITHUB_TOKEN is not set.'
+    exit 1
+fi
+
+git config user.name 'flarum-bot'
+git config user.email 'bot@flarum.org'
+
+cd js
+
+npm i -g npm@6.1.0
+npm ci
+npm run build
+
+git add dist/* -f
+git commit -m "Bundled output for commit $GITHUB_SHA [skip ci]"
+
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" $GITHUB_REF
