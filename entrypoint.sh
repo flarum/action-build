3c40d7cea467d9679ebcfbe0407cf8540687fa17
David Sevilla Martin <me@datitisev.me>
2019-08-21 09:51:42 -0400

    Check if HEAD is behind before committing & pushing, rework style for progress echo's

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -l
 
-set -e
+style="$(tput bold)$(tput setaf 1)$(tput setab 7)"
+reset="$(tput sgr0)"
 
 if [ -z $GITHUB_TOKEN ]; then
-    echo -e '\033[0;31mGITHUB_TOKEN is not set.'
+    echo -e "$(tput setaf 1)GITHUB_TOKEN is not set."
     exit 1
 fi
 
@@ -12,22 +13,33 @@ git config user.email 'bot@flarum.org'
 
 cd js
 
-echo -e '\e[36m\e[1mInstalling dependencies.'
+echo -e "$style - installing dependencies $reset"
 
 npm i -g npm@6.1.0
 npm ci
 
-echo -e '\e[36m\e[1mBuilding JavaScript files.'
+echo -e "$style - building JavaScript files $reset"
 
 npm run build
 
 git add dist/* -f
 
-if [[ -z $(git status -uno --porcelain) ]]; then
-    echo -e '\e[36m\e[1mNothing to commit.'
-else
-    echo -e '\e[36m\e[1mCommitting and pushing.'
+echo -e "$style - checking remote $reset"
 
-    git commit -m "Bundled output for commit $GITHUB_SHA [skip ci]"
-    git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" "HEAD:$GITHUB_REF"
+git fetch origin
+
+behind=$(git log --oneline HEAD..origin | wc -l)
+
+if [[ $behind -gt 0 ]]; then
+    echo -e "$style - HEAD is behind by $behind commit(s) $reset"
+    exit
+elif [[ -z $(git status -uno --porcelain) ]]; then
+    echo -e "$style - nothing to commit $reset"
+    exit
 fi
+
+
+echo -e "$style - committing and pushing $reset"
+
+git commit -m "Bundled output for commit $GITHUB_SHA [skip ci]"
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" "HEAD:$GITHUB_REF"
