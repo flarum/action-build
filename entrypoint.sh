ce3c17048ba6231a7a8da2a02166efee0cb0e7c8
David Wheatley <hi@davwheat.dev>
2021-05-12 23:23:20 +0100

    Add support for building typings, using a different package manager, and specifying a custom build script (#6)

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,32 @@ reset='\e[0;10m'
 trap 'log "$BASH_COMMAND"' DEBUG
 
 log() {
-    # ignore echo commands
-    if [[ $1 != echo* ]]; then
-        echo "##[command]$1"
-    fi
+  # ignore echo commands
+  if [[ $1 != echo* ]]; then
+    echo "##[command]$1"
+  fi
 }
 
-if [ -z "$GITHUB_TOKEN" ]; then
+if [ -z "$INPUT_GITHUB_TOKEN" ]; then
+  # Backwards compatibility with `env` options method
+  if [ -v GITHUB_TOKEN ]; then
+    $INPUT_GITHUB_TOKEN=$GITHUB_TOKEN
+  else
     echo -e "\e[0;31mGITHUB_TOKEN is not set."
     exit 1
+  fi
+fi
+
+if [ -z "$INPUT_BUILD_SCRIPT" ]; then
+  INPUT_BUILD_SCRIPT = "build"
+fi
+
+if [ -z "$INPUT_PACKAGE_MANAGER" ]; then
+  INPUT_PACKAGE_MANAGER = "npm"
+fi
+
+if [ -z "$INPUT_JS_PATH" ]; then
+  INPUT_JS_PATH = "./js"
 fi
 
 # script
@@ -28,40 +45,68 @@ echo -e "$style - setting up git $reset"
 git config user.name 'flarum-bot'
 git config user.email 'bot@flarum.org'
 
-echo -e "$style - installing dependencies $reset"
+echo -e "$style - entering JS path ($INPUT_JS_PATH) $reset"
 
-cd js || exit 1
-npm i -g npm@7.11.2
-npm ci
+cd $INPUT_JS_PATH || exit 1
 
-echo -e "$style - building JavaScript files $reset"
+echo -e "$style - installing dependencies with $INPUT_PACKAGE_MANAGER $reset"
 
-npm run build
+npm i -g npm@^7
+
+if [[ "$INPUT_PACKAGE_MANAGER" == "npm" ]]; then
+  npm ci
+else
+  npm i -g yarn
+  yarn install --frozen-lockfile
+fi
+
+echo -e "$style - building Javascript/Typescript files $reset"
+
+# Equivalent to npm run xxx or yarn run xxx
+$INPUT_PACKAGE_MANAGER run $INPUT_BUILD_SCRIPT
+
+# Build and add typings to staged files
+if [ -v INPUT_TYPINGS_SCRIPT ]; then
+  echo -e "$style - building typings $reset"
+
+  # Typings build often has errors -- let's not exit if we have any issues
+  set +e
+  $INPUT_PACKAGE_MANAGER run $INPUT_TYPINGS_SCRIPT
+  set -e
+
+  git add dist-typings/* -f
+
+  COMMIT_DESC="Includes transpiled JS/TS, and Typescript declaration files (typings)."
+else
+  echo -e "$style - not building typings $reset"
+  COMMIT_DESC="Includes transpiled JS/TS."
+fi
 
 git add dist/* -f
 
 if [[ -z $(git status -uno --porcelain) ]]; then
-    echo -e "$style - nothing to commit $reset"
-    exit
+  echo -e "$style - nothing to commit $reset"
+  exit
 fi
 
 echo -e "$style - committing and pushing $reset"
 
-git commit -m "Bundled output for commit $GITHUB_SHA [skip ci]"
+# Multiple `-m`s are treated as separate paragraphs by git.
+git commit -m "Bundled output for commit $GITHUB_SHA" -m "$COMMIT_DESC" -m "[skip ci]"
 
 # no longer exit on error
 set +e
 
-OUT="$(git push https://"$GITHUB_ACTOR":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git HEAD:"$GITHUB_REF" 2>&1 > /dev/null)"
+OUT="$(git push https://"$GITHUB_ACTOR":"$GITHUB_TOKEN"@github.com/"$GITHUB_REPOSITORY".git HEAD:"$GITHUB_REF" 2>&1 >/dev/null)"
 
-if grep -q "the remote contains work that you do\|a pushed branch tip is behind its remote" <<< "$OUT"; then
-    echo -e "$style - HEAD is behind $reset"
-    exit
-elif grep -q "fatal:\|error:" <<< "$OUT"; then
-    echo -e "$style - error $reset"
-    echo "$OUT"
-    exit 1
+if grep -q "the remote contains work that you do\|a pushed branch tip is behind its remote" <<<"$OUT"; then
+  echo -e "$style - HEAD is behind $reset"
+  exit
+elif grep -q "fatal:\|error:" <<<"$OUT"; then
+  echo -e "$style - error $reset"
+  echo "$OUT"
+  exit 1
 else
-    echo -e "$style - success $reset"
-    echo "$OUT"
+  echo -e "$style - success $reset"
+  echo "$OUT"
 fi
